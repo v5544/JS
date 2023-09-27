34352b9b91b629cb65dd13ba6b6c5c441f713183
Vinay Singh <76996519+v5544@users.noreply.github.com>
2023-09-27 19:27:00 +0000

    Conversion of dataType

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,23 @@
+let score = "Vinay"
+
+// console.log(typeof score);
+// console.log(typeof (score));
+
+let valueInNumber = Number(score);
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+// 33 -> 33
+// "33abc" -> NaN
+// true -> 1 , false -> 0
+
+let isLogedIn 
+let isBoolean = Boolean(isLogedIn);
+// console.log(isBoolean);
+
+// 1 -> true ; 0 -> false ; "" -> false ; " " -> true "vinay" -> true ;  -> false
+
+let num = 33;
+let string = String(num);
+console.log(string);
+console.log(typeof string);
